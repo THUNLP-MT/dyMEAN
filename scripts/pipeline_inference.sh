d8c2e92083f755ebf0b9eef7742d9c98aa9324af
kxz@thumt102-1 <15068701650@163.com>
2023-11-16 16:22:44 +0800

    rename directory

--- scripts/pipeline_inference.sh
+++ scripts/pipeline_inference.sh
@@ -1,8 +1,10 @@
-#!/bin/zsh
-CODE_DIR=`realpath $(dirname "$0")/../..`
+#!/bin/bash
+CODE_DIR=`realpath $(dirname "$0")/..`
 DATA_DIR=${CODE_DIR}/RAbD
 DATASET=${DATA_DIR}/test.json
 CDR=H3
+
+echo "Project Directory: ${CODE_DIR}"
 
 MODEL=$1
 CKPT=$2
@@ -22,7 +24,7 @@ MODEL_CODE_DIR=`echo -e "from configs import ${MODEL}_DIR\nprint(${MODEL}_DIR)" 
 echo "Model source: ${MODEL_CODE_DIR}"
 
 echo "Generating complex templates"
-python -m baselines.pipeline.gen_complex_templates \
+python -m models.pipeline.gen_complex_templates \
     --dataset_json ${DATASET} \
     --cdr_model ${MODEL} \
     --out_dir ${DATA_DIR}/complex_templates \
@@ -32,7 +34,7 @@ python -m baselines.pipeline.gen_complex_templates \
 echo "Inference"
 
 if [ "${MODEL}" = "MEAN" ]; then
-    PYTHONPATH=$MODEL_CODE_DIR python baselines/pipeline/cdr_models/mean_gen.py \
+    PYTHONPATH=$MODEL_CODE_DIR python models/pipeline/cdr_models/mean_gen.py \
         --ckpt ${CKPT} \
         --dataset ${DATA_OUT_DIR}/summary.json \
         --batch_size 32 \
@@ -40,13 +42,13 @@ if [ "${MODEL}" = "MEAN" ]; then
         --gpu 0 \
         --out_dir ${RESULT_DIR}
 elif [ "${MODEL}" = "Rosetta" ]; then
-    PYTHONPATH=$CODE_DIR python baselines/pipeline/cdr_models/rosetta_gen.py \
+    PYTHONPATH=$CODE_DIR python models/pipeline/cdr_models/rosetta_gen.py \
         --dataset ${DATA_OUT_DIR}/summary.json \
         --cdr_type ${CDR} \
         --out_dir ${RESULT_DIR}
 elif [ "${MODEL}" = "DiffAb" ]; then
     conda activate agdiffab
-    PYTHONPATH=$MODEL_CODE_DIR python baselines/pipeline/cdr_models/diffab_gen.py \
+    PYTHONPATH=$MODEL_CODE_DIR python models/pipeline/cdr_models/diffab_gen.py \
         --dataset ${DATA_OUT_DIR}/summary.json \
         --out_dir ${RESULT_DIR} \
         --config ${CKPT}
@@ -55,13 +57,13 @@ fi
 
 echo "Evaluate"
 if [ "${MODEL}" = "Rosetta" ]; then
-    python -m baselines.pipeline.evaluate \
+    python -m models.pipeline.evaluate \
         --summary_json ${DATASET} \
         --ref_dir ${DATA_DIR}/complex_templates \
         --gen_dir ${RESULT_DIR} \
         --cdr_type ${CDR}
 else
-    python -m baselines.pipeline.evaluate \
+    python -m models.pipeline.evaluate \
         --summary_json ${DATASET} \
         --ref_dir ${DATA_DIR}/complex_templates \
         --gen_dir ${RESULT_DIR} \
